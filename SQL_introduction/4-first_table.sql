6d66dfe3d1eb4ae0ece13f40f8e050a07e116f68
kejgon james <kejgon@gmail.com>
2024-01-12 13:22:00 +0300

    commit

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,11 +1,14 @@
--- my first_table
-
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
 
-use hbtn_test_db_4;
+-- Switch to the hbtn_test_db_4 database
+USE hbtn_test_db_4;
 
-
+-- Create the first_table if it doesn't exist
 CREATE TABLE IF NOT EXISTS first_table (
-    -> ID INT,
-    -> NAME VARCHAR(256) ->
+    ID INT,
+    NAME VARCHAR(256)
 );
+
+-- Insert sample data for testing
+-- INSERT INTO first_table (ID, NAME) VALUES (1, 'John'), (2, 'Jane');
