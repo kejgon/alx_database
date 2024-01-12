e2f792b6cacc4c59efd890c37827e79e0f74a5d1
kejgon james <kejgon@gmail.com>
2024-01-12 08:43:10 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,6 +1,7 @@
 
+
 -- # Assuming 'your_database_name' is the argument passed to the script
 DATABASE_NAME="hbtn_0c_0"
 
 -- # Use the MySQL command to list tables in the specified database
-mysql -u root -p -e "USE $DATABASE_NAME; SHOW TABLES;"
+mysql -u root -p -e "USE ${DATABASE_NAME}; SHOW TABLES;"
