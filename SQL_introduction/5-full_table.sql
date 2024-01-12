5a809a6cefcff37e17fa1858f858a6c08f708250
kejgon james <kejgon@gmail.com>
2024-01-12 13:45:23 +0300

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,20 +1,13 @@
--- # Assuming 'your_database_name' is the argument passed to the script
-DATABASE_NAME="$1"
+-- Create database and table
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
 
--- # Define the SQL query to retrieve table information
-TABLE_INFO_SQL="SELECT 
-CONCAT('CREATE TABLE `', table_name, '` (\n',
-GROUP_CONCAT(
-CONCAT('  `', column_name, '` ', column_type, 
-IF(IS_NULLABLE = 'NO', ' NOT NULL', ' DEFAULT NULL')),
-'\n'
-),
-IFNULL(CONCAT(', PRIMARY KEY (`', GROUP_CONCAT(column_name), '`)'), ''),
-'\n) ENGINE=', engine, ' DEFAULT CHARSET=', table_collation, ';\n')
-FROM information_schema.tables
-JOIN information_schema.columns USING (table_schema, table_name)
-
-WHERE table_schema = '$DATABASE_NAME' AND table_name = 'first_table';"
-
--- # Use the MySQL command to execute the query
-MYSQL -H LOCALHOST -U ROOT -P $DATABASE_NAME -E "$TABLE_INFO_SQL"
+DESCRIBE hbtn_test_db_5.first_table;
