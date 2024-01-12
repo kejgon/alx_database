21f253d6836f0e8c1d44372d80f2806185759ab9
kejgon james <kejgon@gmail.com>
2024-01-12 21:05:00 +0300

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -16,5 +16,3 @@ INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
 
--- Display second_table
-SELECT id, name, score FROM second_table ORDER BY id ASC;
