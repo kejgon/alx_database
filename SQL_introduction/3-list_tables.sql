2e6b8c5d32ba324e1c3b7f3bc34cbf47b9261f22
kejgon james <kejgon@gmail.com>
2024-01-12 13:09:35 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,10 +1,11 @@
-
-
 -- # Check for database name argument
 if [ -z "$1" ]; then
-  echo "Error: Please provide the database name as an argument."
-  exit 1
-fi
+
+ECHO "Error: Please provide the database name as an argument."
+
+exit 1
+
+FI
 
 --# Connect to MySQL server
-mysql -hlocalhost -uroot -p -e "SHOW TABLES FROM $1;"
+MYSQL -HLOCALHOST -UROOT -P -E "SHOW TABLES FROM $1;"
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- my first_table
+
+CREATE TABLE IF NOT EXISTS FIRST_TABLE (
+    -> ID INT,
+    -> NAME VARCHAR(256) ->
+);
