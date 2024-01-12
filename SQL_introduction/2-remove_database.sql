8e013288a82eb10d9fd4a57725e11864886a8213
kejgon james <kejgon@gmail.com>
2024-01-12 21:09:51 +0300

    commit

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,8 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Switch to the hbtn_0c_0 database
+USE hbtn_0c_0;
+
 -- Check if the database exists before dropping it
-DROP DATABASE IF EXISTS HBTN_0C_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
