c3ffe022d5a82a8a76c46d9108575bb1bedbbe6e
kejgon james <kejgon@gmail.com>
2024-01-12 08:40:14 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,4 +2,4 @@
 DATABASE_NAME="hbtn_0c_0"
 
 -- # Use the MySQL command to list tables in the specified database
-MYSQL -E "USE $database_name; SHOW TABLES;"
+MYSQL -U ROOT -P -E "USE $DATABASE_NAME; SHOW TABLES;"
