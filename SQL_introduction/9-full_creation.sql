7d7a6b69bd88e54d4bb5c525405e2e5be3e633e9
kejgon james <kejgon@gmail.com>
2024-01-12 21:23:29 +0300

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,18 +1,25 @@
--- Create database
-DROP DATABASE IF EXISTS hbtn_test_db_9;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
-USE hbtn_test_db_9;
 
--- Create table
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
+
+-- # Assuming 'your_database_name' is the argument passed to the script
+DATABASE_NAME="$1"
+
+-- # Define the SQL query to create second_table
+CREATE_TABLE_SQL="CREATE TABLE IF NOT EXISTS $DATABASE_NAME.second_table (
+    id INT NOT NULL,
     name VARCHAR(256),
-    score INT
-);
+    score INT,
+    PRIMARY KEY (id)
+);"
 
--- Create records
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+-- # Use the MySQL command to execute the query
+mysql -h localhost -u root -p $DATABASE_NAME -e "$CREATE_TABLE_SQL"
 
+-- # Define the SQL query to insert rows into second_table
+INSERT_ROWS_SQL="INSERT INTO $DATABASE_NAME.second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);"
+
+-- # Use the MySQL command to execute the query
+mysql -h localhost -u root -p $DATABASE_NAME -e "$INSERT_ROWS_SQL"
