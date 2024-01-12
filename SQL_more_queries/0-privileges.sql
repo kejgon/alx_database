cf0b9f122116487dab8b9b8542c1a252a9e2e2fd
kejgon james <kejgon@gmail.com>
2024-01-12 22:52:21 +0300

    commit

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- Grantting users access
+USER_1="user_0d_1"
+USER_2="user_0d_2"
+LIST_PRIVILEGES_SQL="SHOW GRANTS FOR '$USER_1'@'localhost';
+
+SHOW GRANTS FOR '$USER_2'@'localhost';"
+
+# USE THE MYSQL COMMAND TO EXECUTE THE QUERY
+MYSQL -H LOCALHOST -U ROOT -P -E "$LIST_PRIVILEGES_SQL"
