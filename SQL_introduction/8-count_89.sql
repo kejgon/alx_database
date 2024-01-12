8be4ce1962872ec236e89eb960c35f0a399f3e8a
kejgon james <kejgon@gmail.com>
2024-01-12 19:26:31 +0300

    commit

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,68 +1,25 @@
--- Create database
-DROP DATABASE IF EXISTS HBTN_TEST_DB_8;
-
-CREATE DATABASE IF NOT EXISTS HBTN_TEST_DB_8;
-
-USE HBTN_TEST_DB_8;
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_test_db_8;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
+USE hbtn_test_db_8;
 
 -- Create table
-CREATE TABLE IF NOT EXISTS FIRST_TABLE (
-    ID INT,
-    NAME VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
 );
 
 -- Create records
-INSERT INTO FIRST_TABLE (
-    ID,
-    NAME
-) VALUES (
-    1,
-    "A"
-);
-
-INSERT INTO FIRST_TABLE (
-    ID,
-    NAME
-) VALUES (
-    89,
-    "B"
-);
-
-INSERT INTO FIRST_TABLE (
-    ID,
-    NAME
-) VALUES (
-    2,
-    "C"
-);
-
-INSERT INTO FIRST_TABLE (
-    ID,
-    NAME
-) VALUES (
-    89,
-    "D"
-);
-
-INSERT INTO FIRST_TABLE (
-    ID,
-    NAME
-) VALUES (
-    89,
-    "E"
-);
-
-INSERT INTO FIRST_TABLE (
-    ID,
-    NAME
-) VALUES (
-    3,
-    "F"
-);
+INSERT INTO first_table (id, name) VALUES (1, "A");
+INSERT INTO first_table (id, name) VALUES (89, "B");
+INSERT INTO first_table (id, name) VALUES (2, "C");
+INSERT INTO first_table (id, name) VALUES (89, "D");
+INSERT INTO first_table (id, name) VALUES (89, "E");
+INSERT INTO first_table (id, name) VALUES (3, "F");
 
 SELECT
     COUNT(*)
 FROM
-    $DATABASE_NAME.FIRST_TABLE
+    first_table
 WHERE
     ID = 89;
