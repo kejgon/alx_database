95531ee347ee74b8303b8d14fcffa002e5a26fc6
kejgon james <kejgon@gmail.com>
2024-01-12 13:38:38 +0300

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,20 @@
---# Assuming 'your_database_name' is the argument passed to the script
+-- # Assuming 'your_database_name' is the argument passed to the script
 DATABASE_NAME="$1"
 
---# Define the SQL query to retrieve table information
-TABLE_INFO_SQL="SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
+-- # Define the SQL query to retrieve table information
+TABLE_INFO_SQL="SELECT 
+CONCAT('CREATE TABLE `', table_name, '` (\n',
+GROUP_CONCAT(
+CONCAT('  `', column_name, '` ', column_type, 
+IF(IS_NULLABLE = 'NO', ' NOT NULL', ' DEFAULT NULL')),
+'\n'
+),
+IFNULL(CONCAT(', PRIMARY KEY (`', GROUP_CONCAT(column_name), '`)'), ''),
+'\n) ENGINE=', engine, ' DEFAULT CHARSET=', table_collation, ';\n')
+FROM information_schema.tables
+JOIN information_schema.columns USING (table_schema, table_name)
 
 WHERE table_schema = '$DATABASE_NAME' AND table_name = 'first_table';"
 
---# Use the MySQL command to execute the query
-MYSQL -U ROOT -P -E "$TABLE_INFO_SQL"
+-- # Use the MySQL command to execute the query
+MYSQL -H LOCALHOST -U ROOT -P $DATABASE_NAME -E "$TABLE_INFO_SQL"
