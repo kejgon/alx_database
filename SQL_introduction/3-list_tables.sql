1157743f8c1c03e8a162e543abcc07974e333c67
kejgon james <kejgon@gmail.com>
2024-01-17 13:28:30 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,6 +1,7 @@
--- Delete and create database hbtn_test_db_0 and create 3 tables
+-- Delete and create database hbtn_test_db_0
 DROP DATABASE IF EXISTS hbtn_test_db_0;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+
 USE hbtn_test_db_0;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (
