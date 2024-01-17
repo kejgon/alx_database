4d6b413d28859aacd8022aa8061fa11ca5cfe5ac
kejgon james <kejgon@gmail.com>
2024-01-17 13:30:18 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -4,17 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 
 USE hbtn_test_db_0;
 
-CREATE TABLE IF NOT EXISTS holbteron_0 (
-    id INT
-);
-
-CREATE TABLE IF NOT EXISTS holbteron_1 (
-    name VARCHAR(256)
-);
-
-CREATE TABLE IF NOT EXISTS holbteron_2 (
-    id INT,
-    name VARCHAR(256)
-);
-
 SHOW TABLES FROM hbtn_test_db_0;
