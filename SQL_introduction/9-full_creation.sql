ef727f516e861eceb9ab7227414c154fa3d0a7f6
kejgon james <kejgon@gmail.com>
2024-01-17 13:45:24 +0300

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,15 +1,15 @@
-DROP DATABASE IF EXISTS hbtn_0c_0;
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-USE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_test_db_9;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
+USE hbtn_test_db_9;
 
 -- Create table
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_9 (
     id INT,
     name VARCHAR(256)
 );
 
 -- Create table if not exists
-CREATE TABLE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS hbtn_test_db_9 (
     id INT,
     name VARCHAR(256),
     score INT
