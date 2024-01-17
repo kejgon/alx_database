a59f081ee36c9b04927318400240369715d38a3d
kejgon james <kejgon@gmail.com>
2024-01-17 11:15:36 +0300

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
 -- Grantting users access
-
-
-USER_1="user_0d_1"
-USER_2="user_0d_2"
-MYSQL -H LOCALHOST -U ROOT -P -E "SHOW GRANTS FOR '$USER_1'@'localhost';"
-MYSQL -H LOCALHOST -U ROOT -P -E "SHOW GRANTS FOR '$USER_2'@'localhost';"
+SHOW GRANTS FOR '$USER_1'@'localhost';
+SHOW GRANTS FOR '$USER_2'@'localhost';
