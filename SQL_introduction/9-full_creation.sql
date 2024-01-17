d25c8505ee7c8016deaceed5db10f5f16cc4917d
kejgon james <kejgon@gmail.com>
2024-01-17 13:47:22 +0300

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,13 +3,13 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
 USE hbtn_test_db_9;
 
 -- Create table
-CREATE TABLE IF NOT EXISTS hbtn_test_db_9 (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
 
 -- Create table if not exists
-CREATE TABLE IF NOT EXISTS hbtn_test_db_9 (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
