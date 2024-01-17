57922aafd55606311a27bfcae10bef34ec347402
kejgon james <kejgon@gmail.com>
2024-01-17 12:13:07 +0300

    commit

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,6 +4,20 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Switch to the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS cities ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
+);
+
 -- List all cities of California without using JOIN
 SELECT cities.id, cities.name
 FROM cities, states
