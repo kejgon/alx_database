1325ed20fdba75cc5532061c5e242b6e3382cdb9
kejgon james <kejgon@gmail.com>
2024-01-17 13:40:37 +0300

    commit

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -9,13 +9,7 @@ CREATE TABLE IF NOT EXISTS first_table (
     name VARCHAR(256)
 );
 
--- Create records with INSERT IGNORE
-INSERT IGNORE INTO first_table (id, name) VALUES (1, "A");
-INSERT IGNORE INTO first_table (id, name) VALUES (89, "B");
-INSERT IGNORE INTO first_table (id, name) VALUES (2, "C");
-INSERT IGNORE INTO first_table (id, name) VALUES (89, "D");
-INSERT IGNORE INTO first_table (id, name) VALUES (89, "E");
-INSERT IGNORE INTO first_table (id, name) VALUES (3, "F");
+
 
 -- Query to count records with id 89
 SELECT COUNT(*) FROM first_table WHERE id = 89;
