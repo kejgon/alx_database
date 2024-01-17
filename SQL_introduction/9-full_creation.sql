84763339958f5a1bf984eae9ac438eef94f703c8
kejgon james <kejgon@gmail.com>
2024-01-17 13:52:07 +0300

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -10,3 +10,12 @@ CREATE TABLE IF NOT EXISTS second_table (
     score INT
 );
 
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+-- Create records
+INSERT INTO second_table (id, name, score) VALUES (11, "A", 12);
+INSERT INTO second_table (id, name, score) VALUES (12, "B", 12);
+INSERT INTO second_table (id, name, score) VALUES (13, "C", 12);
+INSERT INTO second_table (id, name, score) VALUES (14, "D", 12);
