a96cf3bd606d30c73bccbd46fca532e19dfb96fd
kejgon james <kejgon@gmail.com>
2024-01-17 13:57:22 +0300

    commit

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,18 +1,14 @@
--- Create database
-DROP DATABASE IF EXISTS hbtn_0c_0;
-
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
-
--- Database name passed as an argument to the script
-USE hbtn_0c_0;
-
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_test_db_7;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_7;
+USE hbtn_test_db_7;
 
 -- Create table
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
+
 
 -- Insert a new row into the first_table
 INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
