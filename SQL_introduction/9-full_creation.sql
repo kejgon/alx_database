2eaa892e498f4f6a6a57169e2059c5ba44b6ed4f
kejgon james <kejgon@gmail.com>
2024-01-17 13:50:31 +0300

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,13 +2,8 @@ DROP DATABASE IF EXISTS hbtn_test_db_9;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
 USE hbtn_test_db_9;
 
--- Create table
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
 
--- Create table if not exists
+-- Create table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
