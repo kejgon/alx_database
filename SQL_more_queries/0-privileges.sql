9a132424977d11fcc6d90e86d54b61b557f4ac9d
kejgon james <kejgon@gmail.com>
2024-01-17 11:18:05 +0300

    commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
 -- Grantting users access
 SHOW GRANTS FOR '$USER_1'@'localhost';
 SHOW GRANTS FOR '$USER_2'@'localhost';
+
+-- Delete user user_0d_1 and user_0d_2 if exist
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
