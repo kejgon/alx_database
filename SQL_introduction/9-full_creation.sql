1ba6aa1a92704bad7bba52753cfc37bc9722103c
kejgon james <kejgon@gmail.com>
2024-01-18 16:31:04 +0300

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,20 +2,14 @@ DROP DATABASE IF EXISTS hbtn_test_db_9;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
 USE hbtn_test_db_9;
 
-
--- Create table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
--- Create records
-INSERT INTO second_table (id, name, score) VALUES (11, "A", 12);
-INSERT INTO second_table (id, name, score) VALUES (12, "B", 12);
-INSERT INTO second_table (id, name, score) VALUES (13, "C", 12);
-INSERT INTO second_table (id, name, score) VALUES (14, "D", 12);
+INSERT INTO second_table (id, name, score) VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
