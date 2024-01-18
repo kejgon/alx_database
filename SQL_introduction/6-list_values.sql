e6e0728b4c29313db9d0140bd72d0d9cd09889b2
kejgon james <kejgon@gmail.com>
2024-01-18 14:43:09 +0300

    commit

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -10,5 +10,9 @@ CREATE TABLE IF NOT EXISTS first_table (
 );
 
 
-
 SELECT * FROM first_table;
+
+-- Insert 3 records
+INSERT INTO first_table (id, name) VALUES (1, "First name");
+INSERT INTO first_table (id, name) VALUES (2, "Last name");
+INSERT INTO first_table (id, name) VALUES (3, "name");
