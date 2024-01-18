1ae1372776d32b8ca473b325c028bb4ffc4ca2a3
kejgon james <kejgon@gmail.com>
2024-01-18 14:31:54 +0300

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,21 +9,5 @@ CREATE TABLE IF NOT EXISTS first_table (
     created_at DATE,
     PRIMARY KEY (id)
 );
--- Get detailed CREATE TABLE statement from information_schema
-SELECT
-    CONCAT('CREATE TABLE `first_table` (',
-           GROUP_CONCAT(
-               CONCAT('`', COLUMN_NAME, '` ', COLUMN_TYPE,
-                      IF(IS_NULLABLE = 'NO', ' NOT NULL', ' DEFAULT NULL'),
-                      IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
-                      IF(EXTRA = 'auto_increment', ' AUTO_INCREMENT', ''),
-                      ','
-               )
-           ),
-           'PRIMARY KEY (`id`)',
-           ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
-    ) AS 'Create Table'
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_test_db_5';
+
+SHOW CREATE TABLE first_table;
