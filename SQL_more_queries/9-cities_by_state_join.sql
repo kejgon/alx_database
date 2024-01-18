8b459aa83f8b41debbf48998e389497f217100c8
kejgon james <kejgon@gmail.com>
2024-01-18 16:49:03 +0300

    commit

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,27 +1,3 @@
--- Create the database hbtn_0d_usa if not exists
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- lists all cities contained in the database hbtn_0d_usa.
 
--- Switch to the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-
--- List all cities with their id, name, and corresponding state name
-SELECT
-    cities.id,
-    cities.name,
-    (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
-FROM cities
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id
