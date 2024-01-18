9a390d6fdf8336a494ec8023877aad8136259142
kejgon james <kejgon@gmail.com>
2024-01-18 11:12:52 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,20 @@
--- Delete and create database hbtn_test_db_0 and create 3 tables
-DROP DATABASE IF EXISTS hbtn_test_db_0;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
-USE hbtn_test_db_0;
+
+--# Check if a database name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+--# Capture the database name from the command line argument
+database_name=$1
+
+--# MySQL command to create and configure the database and tables
+mysql -h localhost -u root -p <<EOF
+-- Delete and create database $database_name
+DROP DATABASE IF EXISTS $database_name;
+CREATE DATABASE IF NOT EXISTS $database_name;
+
+USE $database_name;
 
 CREATE TABLE IF NOT EXISTS holbteron_0 (
     id INT
@@ -17,3 +30,4 @@ CREATE TABLE IF NOT EXISTS holbteron_2 (
 );
 
 SHOW TABLES;
+EOF
