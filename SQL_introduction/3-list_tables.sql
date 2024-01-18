b47de31664e79c80d6ed4d963ca98651c04d9be8
kejgon james <kejgon@gmail.com>
2024-01-18 11:08:05 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,16 @@
--- Delete and create database hbtn_test_db_0
-DROP DATABASE IF EXISTS hbtn_test_db_0;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+-- -- Delete and create database hbtn_test_db_0
+-- DROP DATABASE IF EXISTS hbtn_test_db_0;
+-- CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 
-USE hbtn_test_db_0;
+-- USE hbtn_test_db_0;
 
-SHOW TABLES FROM hbtn_test_db_0;
+-- SHOW TABLES FROM hbtn_test_db_0;
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+USE $database_name; 
+
+SHOW TABLES;
