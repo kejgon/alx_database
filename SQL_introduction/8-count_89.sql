477d972021d987d1c91ef2ee577dce0c6f7b19b6
kejgon james <kejgon@gmail.com>
2024-01-18 16:38:35 +0300

    commit

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,15 +1,9 @@
--- Create database 
-DROP DATABASE IF EXISTS hbtn_test_db_8;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
-USE hbtn_test_db_8;
 
 -- Create table
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
-
-
 
 -- Query to count records with id 89
 SELECT COUNT(*) FROM first_table WHERE id = 89;
