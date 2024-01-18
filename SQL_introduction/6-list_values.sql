f415c044c05977acab31e0859bdc70bb6eb93a0c
kejgon james <kejgon@gmail.com>
2024-01-18 16:41:10 +0300

    commit

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,14 +1,3 @@
+--  lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
--- Create table
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
-
--- Insert 3 records
-INSERT INTO first_table (id, name) VALUES (1, "First name");
-INSERT INTO first_table (id, name) VALUES (2, "Last name");
-INSERT INTO first_table (id, name) VALUES (3, "name");
-
-SELECT * FROM first_table;
-
+SELECT * FROM first_table
