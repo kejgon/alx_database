b9a189f3409fa68f13e6435bcbdfe6c20396070c
kejgon james <kejgon@gmail.com>
2024-01-18 11:17:29 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -6,12 +6,9 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
--- # Capture the database name from the command line argument
 database_name=$1
 
--- # MySQL command to create and configure the database and tables
-mysql -h localhost -u root -p <<EOF
--- Delete and create database $database_name
+
 DROP DATABASE IF EXISTS $database_name;
 CREATE DATABASE IF NOT EXISTS $database_name;
 
@@ -31,4 +28,3 @@ CREATE TABLE IF NOT EXISTS holbteron_2 (
 );
 
 SHOW TABLES;
-EOF
