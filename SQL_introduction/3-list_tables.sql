a2ed5e464e5d425c7c2026f0fba19c81365cae7f
kejgon james <kejgon@gmail.com>
2024-01-18 11:15:17 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,14 +1,15 @@
+-- #!/bin/bash
 
---# Check if a database name is provided as an argument
+-- # Check if a database name is provided as an argument
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <database_name>"
     exit 1
 fi
 
---# Capture the database name from the command line argument
+-- # Capture the database name from the command line argument
 database_name=$1
 
---# MySQL command to create and configure the database and tables
+-- # MySQL command to create and configure the database and tables
 mysql -h localhost -u root -p <<EOF
 -- Delete and create database $database_name
 DROP DATABASE IF EXISTS $database_name;
