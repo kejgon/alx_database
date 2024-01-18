44d23213e815a26d69b9ebe2df5a49a025bba65f
kejgon james <kejgon@gmail.com>
2024-01-18 16:33:49 +0300

    cmmit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -14,3 +14,9 @@ INSERT INTO second_table (id, name, score) VALUES
     (2, "Alex", 3),
     (3, "Bob", 14),
     (4, "George", 8);
+
+-- Create records
+INSERT INTO second_table (id, name, score) VALUES (11, "A", 12);
+INSERT INTO second_table (id, name, score) VALUES (12, "B", 12);
+INSERT INTO second_table (id, name, score) VALUES (13, "C", 12);
+INSERT INTO second_table (id, name, score) VALUES (14, "D", 12);
