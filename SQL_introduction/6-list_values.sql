0f111df056cf99c547a789613a79f80752688040
kejgon james <kejgon@gmail.com>
2024-01-18 16:40:02 +0300

    commit

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,7 +1,3 @@
--- Create database 
-DROP DATABASE IF EXISTS hbtn_test_db_6;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_6;
-USE hbtn_test_db_6;
 
 -- Create table
 CREATE TABLE IF NOT EXISTS first_table (
