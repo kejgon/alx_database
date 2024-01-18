6ffb48ea5fd6921212034cae7d70da0192f9294a
kejgon james <kejgon@gmail.com>
2024-01-18 16:42:43 +0300

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,20 +1,2 @@
--- Delete and create database hbtn_test_db_0
-DROP DATABASE IF EXISTS hbtn_test_db_0;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
-
-USE hbtn_test_db_0;
-
-CREATE TABLE IF NOT EXISTS holbteron_0 (
-    id INT
-);
-
-CREATE TABLE IF NOT EXISTS holbteron_1 (
-    name VARCHAR(256)
-);
-
-CREATE TABLE IF NOT EXISTS holbteron_2 (
-    id INT,
-    name VARCHAR(256)
-);
-
+-- lists all the tables of a database in your MySQL server.
 SHOW TABLES;
